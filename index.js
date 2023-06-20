ef3c153437e6a58714d42ab7de6972cf30e3bf6b
Victor Golovin <rinoally@gmail.com>
2023-06-20 14:38:26 +1000

    add logic for input and button

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 const movies = [];
 
 const addMovieInputNode = document.getElementById("movie-app-input");
+const moviesNode = document.getElementById("movies");
 
 const addMovieButtonNode = document.getElementById("movie-app-button");
 
-
-
 const getMovieFromUser = () => {
-    const movie = addMovieInputNode.ariaValueMax;
+  const movie = addMovieInputNode.ariaValueMax;
 
-    return {
-        movie: movie
-    };
+  return {
+    movie: movie,
+  };
+};
+
+const addMovie = ({ movie }) => {
+  movies.push({
+    movie: movie,
+  });
+};
+
+const getMovies = () => {
+  return movies;
+};
+
+const renderMovies = () => {
+  const movies = getMovies();
+
+  let moviesHTML = "";
+
+  movies.forEach((movie) => {
+    moviesHTML += `
+                <div class='post'>
+                    <p class='movie-name'>${movie.movie}</p> 
+                </div>
+            `;
+  });
+
+  moviesNode.innerHTML = moviesHTML;
 }
 
-const addMovie = () => {
 
-}
 
 const movieHandler = () => {
+  const movieFromUser = getMovieFromUser();
 
-}
+  addMovie(movieFromUser);
+  renderMovies()
+};
 
-addMovieButtonNode = addEventListener("click", movieHandler);
+
+
+addMovieButtonNode.addEventListener("click", movieHandler);
