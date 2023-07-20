34c61c4d0643ada5fb94130c1f4a54c8328121d2
Viktor Golovin <rinoally@gmail.com>
2023-07-20 22:29:11 +0300

    add checkbox in js and css

--- index.js
+++ index.js
@@ -31,26 +31,45 @@ const renderMovies = () => {
 
   movies.forEach((movie) => {
     moviesHTML += `
-                <div class='movie-post'>
-                    <p class='movie-name'>${movie.movie}</p>
-                    <button id="movie-app-close-button" 
-                    class="movie-app-close-button"></button> 
-                </div>
+    <li class="movie-post">
+      <input class="movie-post-checkbox" type="checkbox">
+      <label class="movie-post-label">${movie.movie}</label>
+      <div class="movie-post-delete">
+        <span class="movie-post-delete-span"></span>
+    </div>
+</li>
             `;
   });
 
   moviesNode.innerHTML = moviesHTML;
 }
 
+const deleteMovie = (event) => {
+  if (event.target.classList.contains("film-item__delete-film") ||
+  event.target.classList.contains("film-item__delete-film-span")) {
+      const currentFilmItem = event.target.closest(".film-item");
+      const currentFilmIndex = getFilmIndex(currentFilmItem);
+         
+      films.splice(currentFilmIndex, 1);
+      currentFilmItem.remove();
+      }; 
+
+}
+
 const clearInput = () => {
     addMovieInputNode.value = "";
 };
 
+const emptyInput = () => {
+    if (!addMovieInputNode.value) {
+        return;
+    }
+}
 
 const movieHandler = () => {
   const movieFromUser = getMovieFromUser();
 
-  
+  emptyInput()
   addMovie(movieFromUser);
   renderMovies()
   clearInput()
